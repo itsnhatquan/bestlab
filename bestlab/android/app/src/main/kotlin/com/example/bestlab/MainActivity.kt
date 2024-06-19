62fa757842b4e2358f1c42555304604a727196b2
Quan Tong <nhatquantong@gmail.com>
2024-06-19 17:36:22 +0700

    add project

---
+++ bestlab/android/app/src/main/kotlin/com/example/bestlab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bestlab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ bestlab/lib/pages/login_page.dart
@@ -0,0 +1,94 @@
+
+import 'package:flutter/material.dart';
+import 'package:bestlab/components/my_button.dart';
+import 'package:bestlab/components/my_textfield.dart';
+import 'package:bestlab/components/square_tile.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+
+              // logo
+             Image.asset('lib/images/logo.png',height:293),
+
+
+              // welcome back, you've been missed!
+
+
+              // username textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              // password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              // forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              // sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(height: 50),
+
+              // or continue with
+
+              const SizedBox(height: 50),
+
+              // google + apple sign in buttons
+
+
+              const SizedBox(height: 50),
+
+              // not a member? register now
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
